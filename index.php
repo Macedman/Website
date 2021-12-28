dcaca28ba63cfc84eb6554df205a1e13b7e61cbc
Ec da KreatOr <88333318+Macedman@users.noreply.github.com>
2021-12-28 21:17:53 +0800

    Added images

--- index.php
+++ index.php
@@ -6,15 +6,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Airbcd</title>
     <link rel="icon" type="image/x-icon" href="img/favicon.png">
-    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,400;1,600;1,900&family=Roboto:ital,wght@1,400;1,700&display=swap" rel="stylesheet">
     <!-- Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body><
-    <section>
-        <div class="container-fluid bg-dark"></div>
-    </section>
+<body>
+
+            <!--Navbar-->
+<section id="header">
+    <div class="container">
+    <div class="row">
+    
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-black   ">
+        <a class="navbar-brand text-light" href="#"><img src="img/logo.png" alt="" width="30" height="24" class="align-text-top"><h6>airbcd</a></h6>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon bg-light"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Places to stay</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Experiences</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-light" href="#">Online Experiences</a>
+                    </li>
+                </ul>
+        
+<ul class="navbar-nav navbar-right">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Become a host</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown</a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item text-light text-dark" href="#">Sign up</a></li>
+                    <li><a class="dropdown-item text-light text-dark" href="#">Login</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item text-light text-dark" href="#">Host your home</a></li>
+                    <li><a class="dropdown-item text-light text-dark" href="#">Host an experience</a></li>
+                    <li><a class="dropdown-item text-light text-dark" href="#">Help</a></li>
+                </ul>
+        </div>
+        </nav>
+    
+
+        <div class="row">
+            <div class="col ms-auto">
+                <img src="img/house.png" width="1420px" height="880px"alt=""></img>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+
 
     
+
+
+
+
 </body>
 </html>
